13affcf8978ed6cdb4b04a24fe695e0d7e54135a
Joseph DeWeese <rangerboy901@icloud.com>
2022-09-11 17:36:44 -0500

    Initial Commit

---
+++ DailyWorkout1.0/DailyWorkout1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  DailyWorkout1_0App.swift
+//  DailyWorkout1.0
+//
+//  Created by Joseph Wil;liam DeWeese on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyWorkout1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
